7333bc9c750b15e698e30e40aca16a176cda7a7e
madushanka2016 <madushankakahawa@gmail.com>
2019-11-04 23:16:03 +0530

    init

---
+++ src/main/java/com/group/Main.java
@@ -0,0 +1,11 @@
+package com.group;
+
+import com.group.display.Display;
+
+public class Main {
+    public static void main(String[] args){
+        Display display = new Display();
+        display.show();
+    }
+
+}
